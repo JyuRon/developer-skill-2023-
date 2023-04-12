bd3cb3fd85897ea32bb73f56c0095b3d6d42357b
Jyuka <yoyksks3@gmail.com>
2023-04-12 18:24:34 +0900

    [Common Module] Jpa & postgres 의존성 추가, gradle api 키워드에 대하여
    - id 'java-library' : api 키워드를 사용하기 위함
    - implementation 이 아닌 api 키워드를 사용하는 이유
    (1) implementation은 직접 의존하고 있는 모듈외에는 api가 노출되지 않음
    (2) 즉, api-module에서 common-module 노출되지만, jpa 의존성은 api-module 에 노출되지 않아 save(), findById()등의 메소드를 사용하지 못하는 상황 방지

---
+++ module-common/src/main/java/com/example/modulecommon/domain/Member.java
@@ -0,0 +1,23 @@
+package com.example.modulecommon.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
