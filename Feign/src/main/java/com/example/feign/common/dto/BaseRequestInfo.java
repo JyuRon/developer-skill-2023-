fa1670acca4a675c5c5056bdd93cc16be62c84c9
Jyuka <yoyksks3@gmail.com>
2023-04-14 15:17:50 +0900

    [Feign] Feign Client 사용을 위한 공통 응답, 요청 DTO 생성

---
+++ Feign/src/main/java/com/example/feign/common/dto/BaseRequestInfo.java
@@ -0,0 +1,19 @@
+package com.example.feign.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(Include.NON_NULL)
+public class BaseRequestInfo {
+
+    private String name;
+    private Long age;
+}
