8eef9f555a65a9907bccae32c2ede4757798b4ee
Jyuka <yoyksks3@gmail.com>
2023-04-14 16:06:58 +0900

    [Feign] Feign Client Interceptor 와 FeignClientConfig 의 활용
    (1) interceptor
    - RequestInterceptor 를 상속받아 요청에 대한 로깅처리
    - get 의 경우 queryParam 을 확인 할 수 있다.
    - post 의 경우 responseBody 값을 확인이 가능하며 가공 이후 치환하여 사용 할 수 있다.
    
    (2) config
    - 위 interceptor 작성 이후 bean 등록을 진행
    - DemoFeignConfig의 경우 @FeignClient(configuration = DemoFeignConfig.class) 에서 설장한 클래스
    - 즉 해당 client(demo-client) 에 대한 interceptor 를 추가한 개념

--- Feign/src/main/java/com/example/feign/config/DemoFeignConfig.java
+++ Feign/src/main/java/com/example/feign/config/DemoFeignConfig.java
@@ -1,7 +1,15 @@
 package com.example.feign.config;
 
+import com.example.feign.interceptor.DemoFeignInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DemoFeignConfig {
+
+    @Bean
+    public DemoFeignInterceptor feignInterceptor(){
+        // @RequiredArgsConstructor(staticName = "of") 사용으로 인한 of 메소드 사용
+        return DemoFeignInterceptor.of();
+    }
 }
---
+++ Feign/src/main/java/com/example/feign/interceptor/DemoFeignInterceptor.java
@@ -0,0 +1,34 @@
+package com.example.feign.interceptor;
+
+import feign.Request;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+
+@RequiredArgsConstructor(staticName = "of")
+public class DemoFeignInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+
+        // Feign Client 의 요청이 get 인 경우
+        if(template.method() == Request.HttpMethod.GET.name()){
+            System.out.println("[GET] [DemoFeignInterceptor] queries : " + template.queries());
+            return;
+        }
+
+        // Feign Client 의 요청이 post 인 경우
+        if(template.method() == Request.HttpMethod.POST.name()){
+            String encodedRequestBody = StringUtils.toEncodedString(template.body(), StandardCharsets.UTF_8);
+
+            System.out.println("[POST] [DemoFeignInterceptor] requestBody : " + encodedRequestBody);
+
+            // 추가적으로 본인이 필요한 로직을 추가
+            String convertRequestBody = encodedRequestBody;
+            template.body(convertRequestBody);
+            return;
+        }
+    }
+}
